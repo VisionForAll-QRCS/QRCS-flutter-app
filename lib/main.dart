2e1ec2d7251ea9ec9fcaefcf206de3b6f92ea9f4
AlAnoud2003 <amkhulai@andrew.cmu.edu>
2025-04-05 23:50:30 +0300

    Added the accessibility tool

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //builder: (context, child) => AccessibilityTools(child: child),
+      builder: (context, child) => AccessibilityTools(child: child),
       home: MyHomePage(),
     );
   }
